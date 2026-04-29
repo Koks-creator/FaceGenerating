af4e75751cfc6a54f6b354aca4a30c5b421e8d0f
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2026-04-29 22:45:39 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import json
+from typing import Union
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    # Overall
+    ROOT_PATH: str = Path(__file__).resolve().parent
+
+    # FOLDERS
+    MODELS_PATH: Union[str, os.PathLike, Path] = Path(ROOT_PATH) / "models"
+
+    # FACE GENERATOR
+    LATENT_DIM: int = 100 # static, don't change
+
+    # DATABASE
+    DATABASE_URL: str = f"sqlite:///{Path(ROOT_PATH)}/data/app.db"
+
+    # TOKENS, HASHING
+    SECRET_KEY: str = os.getenv("SECRET_KEY")  # openssl rand -hex 32
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # SETUP USERS
+    ADMIN_LOGIN: str = os.getenv("ADMIN_LOGIN")
+    ADMIN_PASSWORD: str = os.getenv("ADMIN_PASSWORD")
+    WEBAPP_USER_LOGIN: str = os.getenv("WEB_USER")
+    WEBAPP_USER_PASSWORD: str = os.getenv("WEB_USER_PASSWORD")
+
+    # TEST USERS
+    TEST_USER_LOGIN: str = os.getenv("TEST_USER_LOGIN")
+    TEST_USER_PASSWORD: str = os.getenv("TEST_USER_PASSWORD")
+
+    # API
+    API_PORT: int = 5000
+    API_HOST: str = "127.0.0.1"
+    MAX_IMAGE_FILES: int = 5
+    API_LOG_FILE: str = Path(ROOT_PATH) / "logs" / "api_logs.log"
+    API_MODELS_LIST_PATH: Union[str, os.PathLike, Path] = Path(ROOT_PATH) / "api" / "model_to_load.json"
+    API_MODEL_MIN_GEN_LEN: int = 1
+    API_MODEL_MAX_GEN_LEN: int = 32
+
+    # WEB APP
+    WEB_APP_PORT: int = 8000
+    WEB_APP_HOST: str = "127.0.0.1"
+    WEB_APP_DEBUG: bool = True
+    WEB_APP_LOG_FILE: str = Path(ROOT_PATH) / "logs" / "web_app.logs"
+    WEB_APP_TEMP_UPLOADS_FOLDER =  Path(ROOT_PATH) / "webapp" / "static" / "temp_uploads"
+    WEB_APP_FILES_LIFE_TIME: int = 300
+    WEB_APP_USE_SSL: bool = False
+    WEB_APP_SSL_FOLDER: str = f"{ROOT_PATH}/ocr_webapp/ssl_cert"
+    WEB_APP_TESTING: bool = False
+    WEB_APP_LOG_LEVEL: int = logging.DEBUG
+    WEB_API_CHECK_INTERVAL: int = 10
+
+    # LOGGER
+    UVICORN_LOG_CONFIG_PATH: Union[str, os.PathLike, Path] = Path(ROOT_PATH) / "api" / "uvicorn_log_config.json"
+    CLI_LOG_LEVEL: int = logging.DEBUG
+    FILE_LOG_LEVEL: int = logging.DEBUG
+
+    def get_uvicorn_logger(self) -> dict:
+        with open(self.UVICORN_LOG_CONFIG_PATH) as f:
+            log_config = json.load(f)
+            log_config["handlers"]["file_handler"]["filename"] =  Path(self.ROOT_PATH) / "logs" / "api_logs.log"
+            return log_config
---
+++ face_generator.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass, field
+from typing import Union, List, Tuple
+from pathlib import Path
+from tensorflow.keras.models import load_model
+from keras import layers
+import tensorflow as tf
+import numpy as np
+from logging import getLogger
+import matplotlib.pyplot as plt
+
+from custom_decorators import timeit, log_call
+from custom_logger import CustomLogger
+from config import Config
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    def __init__(self, channels, **kwargs):
+        super().__init__(**kwargs)
+        self.channels = channels
+        self.q = layers.Conv2D(channels // 8, 1, padding='same')
+        self.k = layers.Conv2D(channels // 8, 1, padding='same')
+        self.v = layers.Conv2D(channels, 1, padding='same')
+        self.gamma = tf.Variable(0.0, trainable=True, dtype=tf.float32)
+
+    def call(self, x):
+        b = tf.shape(x)[0]
+        h = tf.shape(x)[1]
+        w = tf.shape(x)[2]
+        c = x.shape[3]
+
+        q = tf.reshape(self.q(x), [b, h*w, c//8])
+        k = tf.reshape(self.k(x), [b, h*w, c//8])
+        v = tf.reshape(self.v(x), [b, h*w, c])
+
+        attn = tf.nn.softmax(tf.matmul(q, k, transpose_b=True))
+        out  = tf.matmul(attn, v)
+        out  = tf.reshape(out, [b, h, w, c])
+        return self.gamma * out + x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"channels": self.channels})
+        return config
+
+logger = getLogger("face_geneator")
+
+if not logger.handlers:
+    logger = CustomLogger(logger_log_level=Config.CLI_LOG_LEVEL,
+                        file_handler_log_level=Config.FILE_LOG_LEVEL,
+                        log_file_name=fr"{Config.ROOT_PATH}/logs/face_generator.log",
+                        logger_name="face_geneator"
+                        ).create_logger()
+
+@dataclass
+class FaceGenerator:
+    model_path: Union[str, Path]
+    load_attention: bool = False
+    _latent_dim: int = field(default=Config.LATENT_DIM, init=False)
+
+    @timeit(logger=logger)
+    def __post_init__(self) -> None:
+        logger.info("Initializing FaceGenerator with: \n" \
+        f" - {self.model_path}")
+        try:
+            if self.load_attention:
+                self.generator_model = load_model(self.model_path, custom_objects={
+                    "SelfAttention": SelfAttention
+                })
+            else:
+                self.generator_model = load_model(self.model_path)
+        except Exception as e:
+            logger.error(f"Could not load model from path: {self.model_path}, {e}", exc_info=True)
+    
+    @log_call(logger=logger, hide_res=True, log_params=["num"])
+    @timeit(logger=logger)
+    def generate_faces(self, num: int = 1) -> List[np.ndarray]:
+        noise = tf.random.normal([num, self._latent_dim]) # x rows of 100 random floats
+        generated = self.generator_model(noise, training=False)
+        generated = (generated + 1.0) / 2.0  # [-1,1] → [0,1] - because tahn
+
+        return [gen.numpy() for gen in generated]
+    
+    @staticmethod
+    def show_results(results: List[np.ndarray],
+                     nrows: int = 0,
+                     ncols: int = 0,
+                     figsize: Tuple[int, int] = (6, 6)
+                    ) -> None:
+        res_len = len(results)
+        if not nrows or not ncols:
+            if res_len < 4:
+                ncols = 2
+                nrows = res_len // 2
+            else:
+                ncols = 4
+                nrows = res_len // 4
+
+        fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(results[i])
+            ax.axis('off')
+        plt.tight_layout()
+        plt.show()
+
+
+
+if __name__ == "__main__":
+    face_generator = FaceGenerator(
+        model_path=r"C:\Users\table\PycharmProjects\MojeCos2\FaceGAN\models\wganv4_generator.h5",
+        load_attention=True
+    )
+    res = face_generator.generate_faces(
+        num=20
+    )
+    face_generator.show_results(results=res)
+    # img = res[0]
+    # img = cv2.resize(img, (256, 256))
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # cv2.imshow("res", img)
+    # cv2.waitKey(0)
